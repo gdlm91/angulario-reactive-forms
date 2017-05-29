7eb6fcda7434dafc0c56cf544f3cceab2b229222
Gerardo Leal <lealg@vmware.com>
2017-05-29 15:40:48 -0300

    Save / Revert Methods

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 
+import { HeroService } from './hero.service';
 import { Address, Hero, states } from './data-model';
 
 @Component({
@@ -15,7 +16,7 @@ export class HeroDetailComponent implements OnChanges {
   states = states;
   nameChangeLog: string[] = [];
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private heroService: HeroService) {
     this.createForm();
     this.logNameChange();
   }
@@ -60,6 +61,35 @@ export class HeroDetailComponent implements OnChanges {
     nameControl.valueChanges.forEach(
       (value: string) => this.nameChangeLog.push(value)
     );
+  }
+
+  onSubmit() {
+    this.hero = this.prepareSaveHero();
+    this.heroService.updateHero(this.hero).subscribe(/* error handling */);
+    this.ngOnChanges();
+  }
+
+  prepareSaveHero(): Hero {
+    const formModel = this.heroForm.value;
+
+    // deep copy of form model lairs
+    const secretLairsDeepCopy: Address[] = formModel.secretLairs.map(
+      (address: Address) => Object.assign({}, address)
+    );
+
+    // return new `Hero` object containing a combination of original hero value(s)
+    // and deep copies of changed form model values
+    const saveHero: Hero = {
+      id: this.hero.id,
+      name: formModel.name as string,
+      // addresses: formModel.secretLairs // <-- bad!
+      addresses: secretLairsDeepCopy
+    };
+    return saveHero;
+  }
+
+  revert() {
+    this.ngOnChanges();
   }
 
 }
